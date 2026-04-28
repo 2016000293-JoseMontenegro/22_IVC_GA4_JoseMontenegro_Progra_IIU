51765baed6a49c26b22eabb4d6bb1b7b0bdc9f0a
2016000293-JoseMontenegro <j.montenegro5adb@gmail.com>
2026-04-28 09:47:45 -0600

    Create Lecc10_Act1_Inc1.cs

---
+++ Lecc10_Act1_Inc1.cs
@@ -0,0 +1,25 @@
+//VisualStudio
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int TOTAL_SELLOS = 8;
+        Console.WriteLine("-------CUPONERA DE BARBERÍA DON CRUP-------");
+
+        for (int i = 1; i <= TOTAL_SELLOS; i++)
+        {
+            Console.WriteLine("\n Sello actual {0} de {1} ",i, TOTAL_SELLOS);
+            Console.WriteLine("Presione cualquier tecla para registrar esta visita");
+            Console.ReadKey();//Presionar una tecla antes de continuar
+
+            Console.WriteLine(">>>Sellos #{0} registrado correctamente...");
+        }
+        Console.WriteLine("\n******************************************");
+        Console.WriteLine("¡CUPONERA COMPLETA!");
+        Console.WriteLine("Orozco, has ganado tu corte de cabello GRATIS");
+        Console.WriteLine("\n******************************************");
+
+        Console.WriteLine("\n Presione una tecla para cerrar...");
+        Console.ReadKey();
+    }
+}
